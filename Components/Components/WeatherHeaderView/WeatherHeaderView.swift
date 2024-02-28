8127bf6a3c1d3a7f34f0a5f51a170ad01381db73
Adem Ozsayin <78085814+ademozsayin@users.noreply.github.com>
2024-02-28 15:50:18 +0300

    feat:(Components) - Configure Weather headerView

--- Components/Components/WeatherHeaderView/WeatherHeaderView.swift
+++ Components/Components/WeatherHeaderView/WeatherHeaderView.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
-
+import Networking
 
 // MARK: - WeatherHeaderView
 final public class WeatherHeaderView: UIView, NibOwnerLoadable {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var weatherStatusLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var weatherIcon: UIImageView!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    
     // MARK: - Init
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         loadNibContent()
-        backgroundColor = .systemPink
+    }
+}
+
+// MARK: - Configuration
+public extension WeatherHeaderView {
+    final func configureWith(_ weather: WeatherResponse) {
+        
     }
 }
