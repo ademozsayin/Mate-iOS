0eed0a011e959e49b33e90618e58f5660b502597
Adem Ozsayin <78085814+ademozsayin@users.noreply.github.com>
2024-03-11 00:17:04 +0300

    feat:(Redux) - Add Redux shared_targets to to fix cocoapod .module

---
+++ Redux/Redux/Internal/LogErrorAndExit.swift
@@ -0,0 +1,20 @@
+//
+//  LogErrorAndExit.swift
+//  Redux
+//
+//  Created by Adem Özsayın on 11.03.2024.
+//
+
+import Foundation
+
+
+/// Logs the error in CocoaLumberjack and stops app execution.
+///
+/// Prefer to use this instead of `fatalError()` since messages in fatal errors are not shown
+/// in Sentry. Using this method, Sentry will still only show “Fatal error” in the Issue message
+/// but the `message` can now be accessed through the Encrypted Logging Console.
+///
+public func logErrorAndExit(_ message: String, file: StaticString = #file, line: UInt = #line) -> Never {
+    DDLogError(message)
+    fatalError(message, file: file, line: line)
+}
